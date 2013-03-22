c3a17b17d2c4af4eea4f86630d61aa523a53079d
jackarmley <jack_armley@yahoo.co.uk>
2013-03-22 11:55:53 +0800

    updated less.js to 1.3.3

---
+++ static/scripts/app-min.js
@@ -0,0 +1,28 @@
+/**
+ * HATCHD DIGITAL CLIENT SIDE APP
+ *
+ * DESCRIPTION:
+ *
+ * This client side code...
+ *
+ * This code has been developed in house at HATCHD DIGITAL.
+ *
+ * @author Hatchd Digital <hello@hatchd.com.au>
+ * @see http://hatchd.com.au/
+ * @contributor Jimmy Hillis <jimmy@hatchd.com.au>
+ * @contributor Niaal Holder <niaal@hatchd.com.au>
+ *
+ * FOR DEVELOPERS:
+ *
+ * ALL external libraries and should be imported here, using a buildout
+ * application e.g. CodeKit. This vesion of the file should be pretty,
+ * well formatted, and only contain code that is unique to your OWN app.
+ * Your site should always use /app-min.js when loading, which contains
+ * a minified version of this script prepended with all external scripts.
+ *
+ * STYLE:
+ *
+ * All code should be within 79 characters wide to meet standard Hatchd
+ * protocol. Reformat code cleanly to fit within this tool.
+ *
+ *//*global $, jQuery*//* Application initialization */$(document).ready(function(){});
