ebc43f86b749f0e70230091d5881f87f7b8833de
Neil Ferreira <neil@neilf.net>
2013-06-17 12:15:53 +0800

    Grunted.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -11,11 +11,12 @@
  * @see http://hatchd.com.au/
  * @contributor Jimmy Hillis <jimmy@hatchd.com.au>
  * @contributor Niaal Holder <niaal@hatchd.com.au>
+ * @contributor Neil Ferreira <neil@hatchd.com.au>
  *
  * FOR DEVELOPERS:
  *
  * ALL external libraries and should be imported here, using a buildout
- * application e.g. CodeKit. This vesion of the file should be pretty,
+ * application e.g. CodeKit or Grunt. This vesion of the file should be pretty,
  * well formatted, and only contain code that is unique to your OWN app.
  * Your site should always use /app-min.js when loading, which contains
  * a minified version of this script prepended with all external scripts.
@@ -27,11 +28,15 @@
  *
  */
 
-/*global $, jQuery*/
+require.config({
+    paths: {
+        jquery: '../libs/jquery/jquery'
+    }
+});
 
-/* Application initialization */
+require([], function () {
+    'use strict';
 
-$(document).ready(function () {
-
-
+    // Load correct modules for correct pages
+    require([document.body.id]);
 });
---
+++ static/scripts/homepage.js
@@ -0,0 +1,11 @@
+/*
+ * Homepage view
+ */
+
+/* globals define */
+/* jshint camelcase:false, laxcomma:true */
+define(['jquery'], function ($) {
+    'use strict';
+
+    console.log('Loaded homepage with jQuery version: ' + $.fn.jquery);
+});
