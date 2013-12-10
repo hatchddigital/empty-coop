05bfecc395ed677b6d90b401a2c5813f289d12ea
Jimmy Hillis <jimmy@hillis.me>
2013-12-10 14:32:40 +0800

    Add support for JS files to be loaded based off class names. Key/value object added to match a classname/module.

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,10 +1,6 @@
 /**
  * HATCHD DIGITAL CLIENT SIDE APP
  *
- * DESCRIPTION:
- *
- * This client side code...
- *
  * This code has been developed in house at HATCHD DIGITAL.
  *
  * @author Hatchd Digital <hello@hatchd.com.au>
@@ -15,11 +11,11 @@
  *
  * FOR DEVELOPERS:
  *
- * ALL external libraries and should be imported here, using a buildout
- * application e.g. CodeKit or Grunt. This vesion of the file should be pretty,
- * well formatted, and only contain code that is unique to your OWN app.
- * Your site should always use /app-min.js when loading, which contains
- * a minified version of this script prepended with all external scripts.
+ * ALL external libraries and should be imported here, using a build
+ * script like Grunt. This vesion of the file should be pretty, well
+ * formatted, and only contain code that is unique to your own  app code.
+ * Your site should always use /app-min.js when loading, which contains a
+ * minified version of this script prepended with all.
  *
  * STYLE:
  *
@@ -34,9 +30,26 @@ require.config({
     }
 });
 
-require([], function () {
+require(['jquery'], function ($) {
     'use strict';
 
-    // Load correct modules for correct pages
-    require([document.body.id]);
+    /**
+     * Load view-specific requirements for pages
+     * @return {void}
+     */
+    (function () {
+        var supported_sections = {
+           // 'view-homepage': 'homepage'
+        };
+        // Loop through each body class name to find view specific classes
+        $.each(document.body.className.split(' '), function (index, _class) {
+            if (_class in supported_sections) {
+                require([supported_sections[_class]]);
+            }
+        });
+    }());
+
+    // Sitewide specific code
+    window.console.log('Lock and loaded');
+
 });
