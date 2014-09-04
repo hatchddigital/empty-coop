48e6848e21886aaefd5cd7a911ef20a76771152d
jackarmley <jack_armley@yahoo.co.uk>
2014-09-04 08:34:19 +0800

    added new breakpoint mixin - needs testing

--- static/scripts/breakpointguide.js
+++ static/scripts/breakpointguide.js
@@ -11,25 +11,7 @@ define(['jquery'], function ($) {
                 '<link rel="stylesheet" href="../stylesheets/breakpointguide.css" />'+
                 '<span class="breakpointguide-label"></span>'+
                 '<ul class="breakpointguide-values">'+
-                    '<li class="breakpointguide-value breakpointguide-value--minw">'+
-                        '<span class="breakpointguide-value-label">Min width</span>'+
-                        '<span class="breakpointguide-value-data"></span>'+
-                    '</li>'+
-                    '<li class="breakpointguide-value breakpointguide-value--maxw">'+
-                        '<span class="breakpointguide-value-label">Max width</span>'+
-                        '<span class="breakpointguide-value-data"></span>'+
-                    '</li>'+
-                    '<li class="breakpointguide-value breakpointguide-value--minh">'+
-                        '<span class="breakpointguide-value-label">Min height</span>'+
-                        '<span class="breakpointguide-value-data"></span>'+
-                    '</li>'+
-                    '<li class="breakpointguide-value breakpointguide-value--maxh">'+
-                        '<span class="breakpointguide-value-label">Max height</span>'+
-                        '<span class="breakpointguide-value-data"></span>'+
-                    '</li>'+
-                    '<li class="breakpointguide-value breakpointguide-value--orientation">'+
-                        '<span class="breakpointguide-value-label">Orientation</span>'+
-                        '<span class="breakpointguide-value-data"></span>'+
+                    '<li class="breakpointguide-value">'+
                     '</li>'+
                 '</ul>'+
             '</div>';
