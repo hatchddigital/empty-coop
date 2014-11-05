6a9b453a241c24f92ed5545f97bf5ec9a8ffbb65
jackarmley <jack_armley@yahoo.co.uk>
2014-11-05 16:29:08 +0800

    updated breakpoint guide

--- static/scripts/breakpointguide.js
+++ static/scripts/breakpointguide.js
@@ -4,19 +4,18 @@
 
 define(['jquery'], function ($) {
     'use strict';
-
+    var useguide = true;
     function breakpointguide(){
         var bpgmarkup =
             '<div class="breakpointguide">'+
                 '<link rel="stylesheet" href="../stylesheets/breakpointguide.css" />'+
                 '<span class="breakpointguide-label"></span>'+
-                '<ul class="breakpointguide-values">'+
-                    '<li class="breakpointguide-value">'+
-                    '</li>'+
-                '</ul>'+
+                '<span class="breakpointguide-value"></span>'+
             '</div>';
         $('body').append(bpgmarkup);
     }
-    breakpointguide();
-
+    if(useguide){
+        breakpointguide();
+    }
 });
+
